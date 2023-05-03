72b971655aadfa26f7343993996db8877f9ab0d2
gdelahozatlantic <gdelahoz@atlanticqi.com>
2023-05-03 17:22:29 -0500

    First commit course project

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'newsby-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+
+}
